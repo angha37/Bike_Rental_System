ddac4d73fea9e26775791413b727897cde3e5585
Angha Bhagat <52950824+angha37@users.noreply.github.com>
2024-06-29 13:55:37 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,21 @@
+##Output:-
+##
+##1.Display Stocks
+##2.Rent a Bike
+##3.Exit
+##
+##1
+##Total Bikes 100
+##
+##1.Display Stocks
+##2.Rent a Bike
+##3.Exit
+##
+##2
+##Enter the Qty:...20
+##Total prices 2000
+##Total Bikes 80
+##
+##1.Display Stocks
+##2.Rent a Bike
+##3.Exit
